f2e6dc45e8e492d8154cbec1525ef771a00c3c39
Walter Pesce <pescew@gmail.com>
2024-12-30 21:20:51 -0500

    Add files via upload

---
+++ types/languageTypes.go
@@ -0,0 +1,81 @@
+package types
+
+type LanguageType int
+
+const (
+	LanguageUnknown LanguageType = iota
+	LanguageEnglish
+	LanguageFrench
+	LanguageGerman
+	LanguageItalian
+	LanguageDutch
+	LanguageSwedish
+	LanguageFinnish
+	LanguageSpanish
+	LanguageDanish
+	LanguagePortuguese
+	LanguageCanadianFrench
+	LanguageNorwegian
+	LanguageHebrew
+	LanguageJapanese
+	LanguageRussian
+	LanguageArabic
+	LanguagePolish
+	LanguageGreek
+	LanguageChinese
+	LanguageKorean
+	LanguageNorthAmericanSpanish
+	LanguageTamil
+	LanguageMalay
+	LanguageUnitedKingdom
+	LanguageIcelandic
+	LanguageBelgian
+	LanguageTaiwanese
+)
+
+var languageIDs = [...]string{"000", "001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021", "022", "023", "024", "025", "026", "027"}
+
+var languageTypes = [...]string{
+	"Unknown",
+	"English",
+	"French",
+	"German",
+	"Italian",
+	"Dutch",
+	"Swedish",
+	"Finnish",
+	"Spanish",
+	"Danish",
+	"Portuguese",
+	"Canadian-French",
+	"Norwegian",
+	"Hebrew",
+	"Japanese",
+	"Russian",
+	"Arabic",
+	"Polish",
+	"Greek",
+	"Chinese",
+	"Korean",
+	"North American Spanish",
+	"Tamil",
+	"Malay",
+	"United Kingdom",
+	"Icelandic",
+	"Belgian",
+	"Taiwanese",
+}
+
+func (l LanguageType) ID() string {
+	if int(l) >= len(languageTypes) || int(l) < 0 {
+		return "00"
+	}
+	return languageIDs[l]
+}
+
+func (l LanguageType) String() string {
+	if int(l) >= len(languageTypes) || int(l) < 0 {
+		return "Unknown"
+	}
+	return languageTypes[l]
+}
